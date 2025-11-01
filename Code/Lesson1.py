1adea0c9be0f1387953ba91ad73c60ce1becb25e
Shin-sh0 <shinsh000000@gmail.com>
2025-11-01 17:57:51 +0900

    Lesson1, Lesson2 Complete

---
+++ Code/Lesson1.py
@@ -0,0 +1,7 @@
+print("Hello, Welcome to my Webpage. For your safety, I need your information\n")
+
+name = input("What is your name?\n")
+age = int(input("How old are you?\n")) - 1
+city = input("Where do you live?\n")
+is_information_correct = input(f"Hi, {name}. You are legal age is {age} years old and live in {city}.\nIf it is correct, Please press Y or if it is not correct, Please press N\n[Y/N]")
+print("The answer is " + is_information_correct)
